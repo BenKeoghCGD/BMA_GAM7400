86be99447790cc05ceaf70bf1a0daa45c336d325
GremBenlin <72025283+GremBenlin@users.noreply.github.com>
2024-10-09 11:43:32 +0100

    toolSwitcherUpdaqte
    uses 4 seperate buttons instead of 1

--- Assets/Scripts/Player/ToolSwitcher.cs
+++ Assets/Scripts/Player/ToolSwitcher.cs
@@ -13,13 +13,19 @@ using UnityEngine.UI;
 public class ToolSwitcher : MonoBehaviour
 {
     int selectedTool = 0;
-    public Button toolButton;
+    public Button tool1;
+    public Button tool2;
+    public Button tool3;
+    public Button tool4;
 
     // Start is called before the first frame update
     void Start()
     {
         SelectTool();
-        toolButton.onClick.AddListener(Task);
+        tool1.onClick.AddListener(Task1);
+        tool2.onClick.AddListener(Task2);
+        tool3.onClick.AddListener(Task3);
+        tool4.onClick.AddListener(Task4);
     }
 
     void SelectTool()
@@ -34,14 +40,42 @@ public class ToolSwitcher : MonoBehaviour
         }
     }
 
-    void Task()
+    void Task1()
     {
         // manages the tool switching by cycling through the selected tools and starts again after the last index, if it changes it calls the 'select tool' to update
         int previousSelectedTool = selectedTool;
-        if (selectedTool >= transform.childCount - 1) selectedTool = 0;
-        else selectedTool++;
+        
+        selectedTool= 0;
 
         if (previousSelectedTool != selectedTool) SelectTool();
     }
-   
+
+    void Task2()
+    {
+        // manages the tool switching by cycling through the selected tools and starts again after the last index, if it changes it calls the 'select tool' to update
+        int previousSelectedTool = selectedTool;
+        
+        selectedTool = 1;
+
+        if (previousSelectedTool != selectedTool) SelectTool();
+    }
+    void Task3()
+    {
+        // manages the tool switching by cycling through the selected tools and starts again after the last index, if it changes it calls the 'select tool' to update
+        int previousSelectedTool = selectedTool;
+        
+        selectedTool = 2;
+
+        if (previousSelectedTool != selectedTool) SelectTool();
+    }
+    void Task4()
+    {
+        // manages the tool switching by cycling through the selected tools and starts again after the last index, if it changes it calls the 'select tool' to update
+        int previousSelectedTool = selectedTool;
+        
+        selectedTool = 3;
+
+        if (previousSelectedTool != selectedTool) SelectTool();
+    }
+
 }
