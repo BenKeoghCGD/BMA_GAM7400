226e400a52a4172bee9b679c73601e47f8dd2f6a
GremBenlin <benstott77@gmail.com>
2024-10-18 16:48:54 +0100

    Revert "Merge branch 'main' into BenS_2"
    This reverts commit 9f03d65f377793389268cfe0e07355c518ad4a29, reversing
    changes made to 3df7adde1fd3932b4e69a2d3d8dfd47a87e3e058.

---
+++ Assets/Scripts/Interactable/Objects/Bins/Bin.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Branch: Main (Keogh, Ben)
+ * Commit: c5c64a33b28ef4617eae3f6b5dcc3374872a0938
+ * 
+ * Cleaned 9/10/24 (Keogh, Ben)
+ * Branch: Main, Stable (Keogh, Ben)
+ * Commit: c5c64a33b28ef4617eae3f6b5dcc3374872a0938
+ */
+
+using UnityEngine;
+
+public class Bin : MonoBehaviour, IInteractable
+{
+    [SerializeField] 
+    private LitterType litterType;
+
+    private int _storedLitter = 0;
+
+    public void OnInteract(PlayerScript player)
+    {
+        // Check if litter count is less than 10.
+        if (_storedLitter >= 10)
+        {
+            return;
+        }
+
+        // increase stored amount by player holdage
+        switch (litterType)
+        {
+            case LitterType.Beige:
+                _storedLitter += player.HeldBeigeLitter;
+                break;
+            case LitterType.Red:
+                _storedLitter += player.HeldRedLitter;
+                break;
+            case LitterType.Black:
+                _storedLitter += player.HeldBlackLitter;
+                break;
+            default:
+                _storedLitter += player.HeldBeigeLitter;
+                break;
+        }
+
+
+        Debug.Log("Stored Litter: " + _storedLitter);
+
+        // remove all litter of one type from plauyer
+        player.ClearLitter(litterType);
+        Debug.Log("CLEARED");
+    }
+}
---
+++ Assets/Scripts/Interactable/Objects/Litter/Litter.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Branch: Main (Keogh, Ben)
+ * Commit: c5c64a33b28ef4617eae3f6b5dcc3374872a0938
+ * 
+ * Cleaned 9/10/24 (Keogh, Ben)
+ * Branch: Main, Stable (Keogh, Ben)
+ * Commit: c5c64a33b28ef4617eae3f6b5dcc3374872a0938
+ */
+
+// Updated 14/10/24 (Higham, Ben), changes commented
+
+using UnityEngine;
+
+public class Litter : MonoBehaviour, IInteractable
+{
+    [SerializeField] 
+    private LitterType litterType;
+    [SerializeField] private ToolType requiredTool;
+
+   public ToolType RequiredTool => requiredTool;
+    void Start()
+    {
+        GameManager.GetLitterManager().AddLitter(this);
+    }
+
+    public void SetRequiredTool(ToolType tool)
+    {
+        requiredTool = tool;
+    }
+    public void OnInteract(PlayerScript player)
+    {
+        if (!player.hasRequiredTool(requiredTool))
+        {
+            Debug.Log("incorrect tool");
+            return;
+        }
+        // Adjust player litter count
+        player.AdjustLitter(litterType);
+
+        // Destroy itself, now removed from LitterManager
+        GameManager.GetLitterManager().RemoveLitter(this);
+        Destroy(gameObject);
+    }
+}
